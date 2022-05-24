4c96c77e54d8e05d42dd1dd5dde1d6358a39144b
shikhar sharma <73755581+naruto-o@users.noreply.github.com>
2022-05-24 23:29:12 +0530

    Create Maximum_minimum_in_an_array.java

---
+++ Maximum_minimum_in_an_array.java
@@ -0,0 +1,8 @@
+https://practice.geeksforgeeks.org/problems/find-minimum-and-maximum-element-in-an-array4428/1/
+//easy solution
+class Solution{
+Arrays.sort(a);
+min = a[0];
+max = a[a.length-1];
+return min,max;
+}
